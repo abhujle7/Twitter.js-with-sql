3b1a22af8d5f3e4be1309107d54b37160dfdc61d
Anup Bhujle <Boojlay@Anups-MacBook-Pro.local>
2015-09-28 18:31:26 -0400

    a day's work

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var express = require('express')
-var app = express(); //creates an instance of an express application
+var app = express()
+var fs = require('fs')
+var mime = require('mime')
+var bodyParser = require('body-parser')
 
-app.get('/', function (req, res) {
-	res.send('welcome!')
-})
+var swig = require('swig')
+app.engine('html', swig.renderFile)
+app.set('view engine', 'html')
+app.set('views', process.cwd() + '/views')
+
+app.use(express.static(__dirname + '/public'));
+// static file middleware
+// app.use(function(req, res, next) {
+//   console.log(req.path)
+//   var mimeType = mime.lookup(req.path)
+//   fs.readFile('./public/' + req.path, function(err, fileBuffer) {
+//     if(err) return next()
+//     res.header('Content-Type', mimeType)
+//     res.send(fileBuffer)
+//   })
+// })
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(require('./routes'))
 
 
 
@@ -14,4 +34,5 @@ app.get('/', function (req, res) {
 
 
 
-app.listen(8080, console.log('server listening'))
+
+app.listen(3000)
